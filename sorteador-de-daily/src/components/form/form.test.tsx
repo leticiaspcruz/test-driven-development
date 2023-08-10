91cff0681bc8f8f2fa837929b7ea7292ebc40f40
leticiaspcruz <leticiaspcruz@gmail.com>
2023-08-10 17:50:21 -0300

    task: add describe at form.test

--- sorteador-de-daily/src/components/form/form.test.tsx
+++ sorteador-de-daily/src/components/form/form.test.tsx
@@ -3,68 +3,70 @@ import { fireEvent, render, screen, act } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import Form from './form';
 
-test('if input is empty new participants cannot be added', () => {
-    render(<Form />);
-    const input = screen.getByPlaceholderText('Insira os nomes dos colaboradores');
-    const button = screen.getByRole('button');
-
-    expect(input).toBeInTheDocument();
-    expect(button).toBeDisabled();
-});
-
-test('add collaborator when input is filled', () => {
-    render(<Form />);
-    const input = screen.getByPlaceholderText('Insira os nomes dos colaboradores');
-    const button = screen.getByRole('button');
-
-    fireEvent.change(input, {
-        target: {
-            value: 'Letícia'
-        }
-    });
-
-    fireEvent.click(button)
-
-    expect(input).not.toHaveFocus();
-    expect(input).toHaveValue("");
-});
-
-test('duplicate names are not allowed', () => {
-    render(<Form />);
-    const input = screen.getByPlaceholderText('Insira os nomes dos colaboradores');
-    const button = screen.getByRole('button');
+describe('test Form component behavior', () => {
+    test('if input is empty new participants cannot be added', () => {
+        render(<Form />);
+        const input = screen.getByPlaceholderText('Insira os nomes dos colaboradores');
+        const button = screen.getByRole('button');
     
-    userEvent.type(input, 'Letícia');
-    fireEvent.click(button);
-    userEvent.type(input, 'Letícia');
-    fireEvent.click(button);
-
-    const errorMessage = screen.getByRole('alert');
-    expect(errorMessage).toHaveTextContent('Nomes duplicados não são permitidos!');
-});
-
-test('the error message have to disappear after X seconds', () => {
-    jest.useFakeTimers();
-    render(<Form />);
-    const input = screen.getByPlaceholderText('Insira os nomes dos colaboradores');
-    const button = screen.getByRole('button');
-    
-    userEvent.type(input, 'Letícia');
-    fireEvent.click(button);
-    userEvent.type(input, 'Letícia');
-    fireEvent.click(button);
-
-    let errorMessage = screen.queryByRole('alert');
-    expect(errorMessage).toHaveTextContent('Nomes duplicados não são permitidos!');
-    
-    act(() => {
-        jest.advanceTimersByTime(5000);
+        expect(input).toBeInTheDocument();
+        expect(button).toBeDisabled();
     });
-
-    act(() => {
-        jest.runAllTimers();
+    
+    test('add collaborator when input is filled', () => {
+        render(<Form />);
+        const input = screen.getByPlaceholderText('Insira os nomes dos colaboradores');
+        const button = screen.getByRole('button');
+    
+        fireEvent.change(input, {
+            target: {
+                value: 'Letícia'
+            }
+        });
+    
+        fireEvent.click(button)
+    
+        expect(input).not.toHaveFocus();
+        expect(input).toHaveValue("");
     });
-
-    errorMessage = screen.queryByRole('alert');
-    expect(errorMessage).toBeNull();
+    
+    test('duplicate names are not allowed', () => {
+        render(<Form />);
+        const input = screen.getByPlaceholderText('Insira os nomes dos colaboradores');
+        const button = screen.getByRole('button');
+        
+        userEvent.type(input, 'Letícia');
+        fireEvent.click(button);
+        userEvent.type(input, 'Letícia');
+        fireEvent.click(button);
+    
+        const errorMessage = screen.getByRole('alert');
+        expect(errorMessage).toHaveTextContent('Nomes duplicados não são permitidos!');
+    });
+    
+    test('the error message have to disappear after 5 seconds', () => {
+        jest.useFakeTimers();
+        render(<Form />);
+        const input = screen.getByPlaceholderText('Insira os nomes dos colaboradores');
+        const button = screen.getByRole('button');
+        
+        userEvent.type(input, 'Letícia');
+        fireEvent.click(button);
+        userEvent.type(input, 'Letícia');
+        fireEvent.click(button);
+    
+        let errorMessage = screen.queryByRole('alert');
+        expect(errorMessage).toHaveTextContent('Nomes duplicados não são permitidos!');
+        
+        act(() => {
+            jest.advanceTimersByTime(5000);
+        });
+    
+        act(() => {
+            jest.runAllTimers();
+        });
+    
+        errorMessage = screen.queryByRole('alert');
+        expect(errorMessage).toBeNull();
+    });
 });
