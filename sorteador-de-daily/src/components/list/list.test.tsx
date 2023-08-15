2dde0ba26631ad7b9ed14be252cd0a27ea06c638
leticiaspcruz <leticiaspcruz@gmail.com>
2023-08-15 10:30:20 -0300

    removing comments

--- sorteador-de-daily/src/components/list/list.test.tsx
+++ sorteador-de-daily/src/components/list/list.test.tsx
@@ -3,13 +3,10 @@ import { render, screen } from '@testing-library/react';
 import List from './list';
 import useCollaboratorsList from '../../hooks/collaboratorsList';
 
-//mocking our hook
 jest.mock('../../hooks/collaboratorsList');
 
 describe('test List component behavior', () => {
     test('the list must be empty', () => {
-
-        //simulate the hook usage with mockReturnValueOnce to set the collaborators value before render the component
         (useCollaboratorsList as jest.Mock).mockReturnValueOnce({ collaborators: [] });
 
         render(<List />);
@@ -20,15 +17,11 @@ describe('test List component behavior', () => {
 
     test('the list must be filled with the collaborators names', () => {
         const collaborators = ['Leticia', 'Argel', 'Vitor', 'Bruna', 'Alanis', 'Wiu'];
-
-        //simulate the hook usage with mockReturnValueOnce to set the collaborators value before render the component
         (useCollaboratorsList as jest.Mock).mockReturnValueOnce({ collaborators });
 
         render(<List />);
 
-        //find the list items
         const items = screen.queryAllByRole('listitem');
-        //validate the list filled
         expect(items).toHaveLength(collaborators.length);
     });
 });
