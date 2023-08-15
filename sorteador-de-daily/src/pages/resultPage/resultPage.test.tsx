a813490fab7416d18bfe59a2dd00ef4867b7cffa
leticiaspcruz <leticiaspcruz@gmail.com>
2023-08-15 15:58:56 -0300

    fix: fixing useShuffle mock at test

--- sorteador-de-daily/src/pages/resultPage/resultPage.test.tsx
+++ sorteador-de-daily/src/pages/resultPage/resultPage.test.tsx
@@ -2,17 +2,17 @@ import React from 'react';
 import { screen, render } from '@testing-library/react';
 import useCollaboratorsList from '../../hooks/collaboratorsList';
 import useShuffle from '../../hooks/shuffle';
-
 import ResultPage from './resultPage';
 
 jest.mock('../../hooks/collaboratorsList');
+jest.mock('../../hooks/shuffle');
 
 describe('test ResultPage', () => {
     test('render the daily host', () => {
         const collaborators = ['Leticia', 'Argel', 'Vitor', 'Bruna', 'Alanis', 'Wiu'];
-
-        const shuffledCollaborators = useShuffle(collaborators);
-        (useCollaboratorsList as jest.Mock).mockReturnValueOnce({ collaborators: shuffledCollaborators });
+        const shuffledCollaborators = [ 'Wiu', 'Vitor', 'Leticia', 'Argel', 'Alanis', 'Bruna'];
+        (useCollaboratorsList as jest.Mock).mockReturnValueOnce({ collaborators: collaborators });
+        (useShuffle as jest.Mock).mockReturnValueOnce(shuffledCollaborators);
 
         render(<ResultPage />)
 
