3a87fa1a0197873528f4e2423fc42d8c70b7c2b8
leticiaspcruz <leticiaspcruz@gmail.com>
2023-09-18 10:24:59 -0300

    adjust at shuffle function

--- sorteador-de-daily/src/utils/shuffleCollaborators.ts
+++ sorteador-de-daily/src/utils/shuffleCollaborators.ts
@@ -1,5 +1,10 @@
-export const shuffledCollaborators = ( collaborators:  string[]) => {
-    const shuffledCollaborators = [...collaborators].sort(() => Math.random() - 0.5);
+export const shuffledCollaborators = (collaborators: string[]) => {
+    const shuffledCollaborators = [...collaborators];
+
+    for (let i = shuffledCollaborators.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [shuffledCollaborators[i], shuffledCollaborators[j]] = [shuffledCollaborators[j], shuffledCollaborators[i]];
+    }
 
     return shuffledCollaborators;
 };
